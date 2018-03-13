5de4870066b8c230301d09a9b308505a2d29cf43
Fabio Tust <fabiotust@gmail.com>
2018-03-13 18:26:28 -0300

    Add login and JWT

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = 3000; //porta padrão
 const mysql = require('mysql');
-
+const jwt = require('jsonwebtoken');
 
 //db config 
 const connection = mysql.createConnection({
@@ -71,7 +71,7 @@ router.get('/client/:id?', (req, res) =>{
 })
 
 //rota para criar/editar cliente 
-router.patch('/client/:id?', (req, response) =>{
+router.post('/client/:id?', (req, response) =>{
 
     const email     = req.body.email;
     const name      = req.body.name;
@@ -121,25 +121,6 @@ router.delete('/client/:id?', (req, res) =>{
     }
 })
 
-//rota para mostrar um filme pelo id
-router.get('/movie/:id?', (req, res) =>{
-    let client_id = '';
-    if(req.params.id) {
-        client_id = parseInt(req.params.id);
-        //execSQLQuery('SELECT * FROM movies WHERE id = ?', client_id, res);
-        sql = 'SELECT * FROM movies WHERE id = ?';
-        connection.query(sql, client_id, function(error, results, fields){
-            if(error) 
-              res.json(error);
-            else
-              res.json(results);
-            connection.end();
-            console.log('executou!');
-        });
-
-    }
-})
-
 //rota para lista filmes
 router.get('/movies', (req, res) =>{
     //execSQLQuery('SELECT * FROM movies', res);
@@ -155,134 +136,224 @@ router.get('/movies', (req, res) =>{
 })
 
 //rota para lista filmes
-router.get('/avaliable_movies', (req, res) =>{
-    //execSQLQuery('SELECT * FROM movies', res);
-    sql = 'SELECT * FROM movies WHERE client_id IS NULL';
-    connection.query(sql, function(error, results, fields){
-        if(error) 
-          res.json(error);
-        else
-          res.json(results);
-        connection.end();
-        console.log('executou!');
+router.get('/avaliable_movies', ensureToken, (req, res) =>{
+    jwt.verify(req.token, 'secret', function(err, data) {
+        if (err) {
+            res.sendStatus(403);
+        } else {   
+            sql = 'SELECT * FROM movies WHERE client_id IS NULL';
+            connection.query(sql, function(error, results, fields){
+                if(error) {
+                    res.sendStatus(404);
+                }else{
+                    res.json(results);
+                }
+                connection.end();
+            });
+        }
     });
 })
 
-//rota para mostrar um filme pelo nome
-router.get('/movie/:title?', (req, res) =>{
-    let title = '';
-    if(req.params.title) {
-        title = '%' + req.params.title + '%';
-        console.log(title);
-        //execSQLQuery('SELECT * FROM movies WHERE title ilike ?', title, res);
-        sql = 'SELECT * FROM movies WHERE lower(title) like lower(?)';
-        console.log(sql);
-        connection.query(sql, title, function(error, results, fields){
-            if(error) 
-              res.json(error);
-            else
-              res.json(results);
-            connection.end();
-            console.log('executou!');
-        });
+//rota para mostrar um filme pelo titulo
+router.get('/movie/:title?', ensureToken, (req, res) =>{
+    jwt.verify(req.token, 'secret', function(err, data) {
+        if (err) {
+            res.sendStatus(403);
+        } else {    
+            let title = '';
+            if(req.params.title) {
+                title = '%' + req.params.title + '%';
+                console.log(title);
+                sql = 'SELECT * FROM movies WHERE lower(title) like lower(?)';
+                console.log(sql);
+                connection.query(sql, title, function(error, results, fields){
+                    if(error){
+                        //res.json(error);
+                        res.sendStatus(404);
+                    }else{
+                        res.json(results);  
+                        //res.sendStatus(200);
+                    }
+                    connection.end();
+                });
 
-    }
+            }
+        }
+    });
 })
   
 //rota para criar/editar um filme 
-router.patch('/movie/:id?', (req, response) =>{
+router.post('/movie/:id?', ensureToken, (req, response) =>{
+    jwt.verify(req.token, 'secret', function(err, data) {
+        if (err) {
+            response.sendStatus(403);
+        } else {
+            const title     = req.body.title;
+            const director  = req.body.director;
+            if(req.params.id) {
+                const id = parseInt(req.params.id);
 
-    const title     = req.body.title;
-    const director  = req.body.director;
-    console.log('------');
-    console.log(title);
-    console.log(director);
-    console.log('------');
-    if(req.params.id) {
-        const id = parseInt(req.params.id);
-        console.log(id);
-        var client = [{ title: title, director: director}, id];
-        var sql = 'UPDATE movies SET ? WHERE id = ?';
-    }else{
-        var client = { title: title, director: director};
-        var sql = 'INSERT INTO movies SET ?';
-    }
-    connection.query(sql, client, (err, res) => {
-    if(err){ 
-        response.json(err);
-    }else{
-        response.json(res);
-        console.log('Last insert ID:', res.insertId);
-    }
-    
+                var client = [{ title: title, director: director}, id];
+                var sql = 'UPDATE movies SET ? WHERE id = ?';
+            }else{
+                var client = { title: title, director: director};
+                var sql = 'INSERT INTO movies SET ?';
+            }
+            connection.query(sql, client, (err, res) => {
+                if(err){ 
+                    //response.json(err);
+                    response.sendStatus(404);
+                }else{
+                    //response.json(res);
+                    response.sendStatus(200);
+                }
+            
+            });
+        }
     });
 })
 
 //rota para excluir um filme pelo id
-router.delete('/movie/:id?', (req, res) =>{
-    let movie_id = '';
-    if(req.params.id) {
-        movie_id = parseInt(req.params.id);
-       
-        // TODO: verificar se o cliente tem filmes nao devolvidos 
- 
-        sql = 'DELETE FROM movies WHERE id = ?';
-        connection.query(sql, movie_id, function(error, results, fields){
-            if(error) 
-              res.json(error);
-            else
-              res.json(results);
-            connection.end();
-        });
-
-    }
+router.delete('/movie/:id?', ensureToken, (req, res) =>{
+    jwt.verify(req.token, 'secret', function(err, data) {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            let movie_id = '';
+            if(req.params.id) {
+                movie_id = parseInt(req.params.id);
+            
+                // TODO: verificar se o cliente tem filmes nao devolvidos 
+        
+                sql = 'DELETE FROM movies WHERE id = ?';
+                connection.query(sql, movie_id, function(error, results, fields){
+                    // TODO: testar se deletou algo e retornar adequadamente
+                    
+                    if(error) {
+                        //res.json(error);
+                        res.sendStatus(404);
+                    }else{
+                        //res.json(results);
+                        res.sendStatus(200); 
+                    }        
+                    connection.end();
+                });
+            }
+        }
+    });
 })
 
 //rota para alugar um filme 
-router.patch('/rent_movie/:id?', (req, response) =>{
-
-    const client_id = req.body.client_id;
-    
-    // TODO: testar se o filme já está com algum cliente
-    
-    if(req.params.id) {
-        const id = parseInt(req.params.id);
-        console.log(id);
-        var client = [{ client_id: client_id}, id];
-        var sql = 'UPDATE movies SET ? WHERE id = ?';
-
-    }
-    connection.query(sql, client, (err, res) => {
-    if(err){ 
-        response.json(err);
-    }else{
-        response.json(res);
-        console.log('Last insert ID:', res.insertId);
-    }
-    
+router.post('/rent_movie/:id?', ensureToken, (req, response) =>{
+    jwt.verify(req.token, 'secret', function(err, data) {
+        if (err) {
+            response.sendStatus(403);
+        } else {
+            const client_id = req.body.client_id;
+            
+            // TODO: testar se o filme já está com algum cliente
+            
+            if(req.params.id) {
+                const id = parseInt(req.params.id);
+                var client = [{ client_id: client_id}, id];
+                var sql = 'UPDATE movies SET ? WHERE id = ?';
+            }
+            connection.query(sql, client, (err, res) => {
+                if(err){ 
+                    //response.json(err);
+                    response.sendStatus(404);
+                }else{
+                    //response.json(res);
+                    response.sendStatus(200); 
+                }
+            });
+        }
     });
 })
 
 //rota para devolver um filme 
-router.patch('/return_movie/:id?', (req, response) =>{
+router.post('/return_movie/:id?', ensureToken, (req, response) =>{
+    jwt.verify(req.token, 'secret', function(err, data) {
+        if (err) {
+            response.sendStatus(403);
+        } else {
+            if(req.params.id) {
+                const id = parseInt(req.params.id);
+                console.log(id);
+                var client = [id];
+                var sql = 'UPDATE movies SET client_id = NULL WHERE id = ?';
+            }
+            connection.query(sql, client, (err, res) => {
+            if(err){ 
+                //response.json(err);
+                response.sendStatus(404);
+            }else{
+                //response.json(res);  
+                response.sendStatus(200);              
+            }
+            
+            });
+        }
+    });
+})
 
-    if(req.params.id) {
-        const id = parseInt(req.params.id);
-        console.log(id);
-        var client = [id];
-        var sql = 'UPDATE movies SET client_id = NULL WHERE id = ?';
+//rota para o login 
+router.post('/login', function(req, response) {
 
+    if(req.body.email && req.body.password) {
+        const email = req.body.email;
+        const password = req.body.password;
+        var client = [email, password];
+        var sql = 'SELECT email FROM clients WHERE email = ? AND password = ?';
+    }else{
+        // TODO: melhorar retorno
+        response.sendStatus(403);
     }
     connection.query(sql, client, (err, res) => {
     if(err){ 
-        response.json(err);
+        response.sendStatus(403);
+        //response.json(err);
     }else{
-        response.json(res);
-        console.log('Last insert ID:', res.insertId);
+        if (res.length<1) {
+            response.sendStatus(403);
+        }else{
+            //response.sendStatus(200);
+            const token = jwt.sign({email: res.email}, 'secret');
+            response.json({
+                token: token
+            });
+        }
     }
-    
     });
-})
+    // const user = {id: 3};
+    // const token = jwt.sign({ user }, 'secret');
+    // res.json({
+    //     token: token
+    // });
+});
+
+app.get('/protected', ensureToken, function(req, res) {
+    jwt.verify(req.token, 'secret', function(err, data) {
+      if (err) {
+        res.sendStatus(403);
+      } else {
+        res.json({
+          description: 'Protected information. Congrats!'
+        });
+      }
+    });
+  });
+  
+  function ensureToken(req, res, next) {
+    const header = req.headers["authorization"];
+    if (typeof header !== 'undefined') {
+      req.token = header;
+      next();
+    } else {
+      res.sendStatus(403);
+    }
+  }
+
 
 //inicia o servidor
 app.listen(port);
